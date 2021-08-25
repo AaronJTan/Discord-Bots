690a9d7564a6ca9cbc03f83ecec3f5ee38c49b80
Jacob <aaron@Jacobs-MacBook-Pro.local>
2021-08-25 10:55:37 -0400

    MotivationBot initial commit

---
+++ MotivationBot/motivationbot.py
@@ -0,0 +1,14 @@
+import os
+import requests
+
+from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+bot = commands.Bot(command_prefix='!')
+
+bot.run(TOKEN)
+
+
